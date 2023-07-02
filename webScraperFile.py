bc86eb4cfbe01d04fdb3381156b5f53f9c55abf4
juleswg23 <jiw1@williams.edu>
2023-07-02 15:54:08 -0400

    working webscraper basic

--- webScraperFile.py
+++ webScraperFile.py
@@ -3,39 +3,54 @@ from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 
-url = "https://www.christies.com/auction/auction-22044-nyr/browse-lots"
+url = "https://www.christies.com/auction/auction-22044-nyr/browse-lots?loadall=true&page=2&sortby=lotnumber"
 
 # Extract the host from the URL
+print("0")
 parsed_url = urlparse(url)
 host = parsed_url.netloc
 
-# Configure the WebDriver
-driver_path = "/path/to/chromedriver"  # Replace with the actual path to the WebDriver executable
-driver = webdriver.Chrome(executable_path=driver_path)  # Replace with the appropriate WebDriver class and path
+driver = webdriver.Firefox()  # Replace with the appropriate WebDriver class and path
+
+print("0.5")
 
 # Navigate to the webpage
 driver.get(url)
+print("1")
 
 # Get the page source
 page_source = driver.page_source
 
 # Create a BeautifulSoup object from the page source
 soup = BeautifulSoup(page_source, "html.parser")
+print("2")
 
 qualifying_lots = []
 non_qualifying_lots = []
 
-lots = soup.find_all("div", class_="lot-details-container")
+paintings = soup.find_all("div", class_="chr-lot-tile__content")
+#chr-lot-tile__content to see if both realized and estimate exist
+
+lots = soup.find_all("div", class_="chr-lot-tile__price-container")
+
+curRealized = None
+curEstimate = None
+
 for lot in lots:
-    estimate = lot.find("span", class_="estimate-value")
-    price_realized = lot.find("span", class_="price-realized-value")
-    if estimate and price_realized:
-        estimate_price = float(estimate.text.strip().replace(",", "").replace("$", ""))
-        price_realized_value = float(price_realized.text.strip().replace(",", "").replace("$", ""))
-        if price_realized_value >= (5 * estimate_price):
-            qualifying_lots.append(lot)
-        else:
-            non_qualifying_lots.append(lot)
+    #loop throught paintings with another find call in here
+    if "chr-lot-tile__price-container--secondary" in lot["class"]:
+        curRealized = lot.find("span", class_="chr-lot-tile__secondary-price-value").text
+
+        parsedEstimate = curEstimate[curEstimate.find("-")+1:].strip()
+        numberEstimate = int(parsedEstimate.replace(",", ""))
+        parsedRealized = curRealized[curRealized.find("D")+1:].strip()
+        numberRealized = int(parsedRealized.replace(",", ""))
+        print(parsedEstimate + ", " + parsedRealized)
+        if numberEstimate * 5 <= numberRealized:
+            print("FOUND ONE")
+        continue
+    curEstimate = lot.find("span", class_="chr-lot-tile__price-value").text
+
 
 print("Qualifying Lots:")
 for lot in qualifying_lots:
